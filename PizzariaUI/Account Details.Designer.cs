bb505669d71feadd5962d75f4859b589a62afd1e
YagnaPatel99 <61169403+YagnaPatel99@users.noreply.github.com>
2020-04-21 00:22:05 -0400

    my bad sir

--- PizzariaUI/Account Details.Designer.cs
+++ PizzariaUI/Account Details.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(842, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1021, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Deals";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1155, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Order History";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1292, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 47);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Account Details";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Account_Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1444, 803);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Account_Details";
             this.Text = "Account_Details";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
