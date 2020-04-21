0fc6f0b20fa7916178357d0ef8ae481d211a2629
jamesrivett <jamesrivett@users.noreply.github.com>
2020-04-21 15:39:43 -0400

    master update

--- PizzariaUI/Employee Login.cs
+++ PizzariaUI/Employee Login.cs
@@ -16,5 +16,10 @@ namespace PizzariaUI
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
