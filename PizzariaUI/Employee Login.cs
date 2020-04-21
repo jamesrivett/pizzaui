d08061f95cfebd529ffb6be70361c56f3e2d0c70
Jorge <43802790+jorgecab796@users.noreply.github.com>
2020-04-21 16:30:03 -0400

    f
    f

--- PizzariaUI/Employee Login.cs
+++ PizzariaUI/Employee Login.cs
@@ -21,5 +21,25 @@ namespace PizzariaUI
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
