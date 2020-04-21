d1ff4760aa4ee437642f05edc2d708e7cdde776e
jamesrivett <jamesrivett@users.noreply.github.com>
2020-04-20 21:56:56 -0400

    asdf

--- PizzariaUI/New Customer.Designer.cs
+++ PizzariaUI/New Customer.Designer.cs
@@ -51,18 +51,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Location = new System.Drawing.Point(90, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(404, 58);
+            this.label1.Size = new System.Drawing.Size(222, 116);
             this.label1.TabIndex = 1;
-            this.label1.Text = "WELCOME TO THE FAMILY";
+            this.label1.Text = "WELCOME TO\r\nTHE FAMILY";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(323, 88);
+            this.label2.Location = new System.Drawing.Point(207, 147);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(91, 19);
             this.label2.TabIndex = 4;
@@ -72,7 +72,7 @@
             // loginButton
             // 
             this.loginButton.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginButton.Location = new System.Drawing.Point(151, 424);
+            this.loginButton.Location = new System.Drawing.Point(35, 483);
             this.loginButton.Name = "loginButton";
             this.loginButton.Size = new System.Drawing.Size(246, 57);
             this.loginButton.TabIndex = 6;
@@ -84,7 +84,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(305, 241);
+            this.label3.Location = new System.Drawing.Point(189, 300);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(109, 19);
             this.label3.TabIndex = 7;
@@ -94,7 +94,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(325, 139);
+            this.label4.Location = new System.Drawing.Point(209, 198);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(89, 19);
             this.label4.TabIndex = 8;
@@ -104,7 +104,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(316, 190);
+            this.label5.Location = new System.Drawing.Point(200, 249);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(98, 19);
             this.label5.TabIndex = 9;
@@ -114,7 +114,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(331, 292);
+            this.label6.Location = new System.Drawing.Point(215, 351);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(83, 19);
             this.label6.TabIndex = 10;
@@ -123,7 +123,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(128, 110);
+            this.textBox1.Location = new System.Drawing.Point(12, 169);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(286, 26);
             this.textBox1.TabIndex = 11;
@@ -131,7 +131,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(128, 161);
+            this.textBox2.Location = new System.Drawing.Point(12, 220);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(286, 26);
             this.textBox2.TabIndex = 12;
@@ -139,7 +139,7 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(128, 212);
+            this.textBox3.Location = new System.Drawing.Point(12, 271);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(286, 26);
             this.textBox3.TabIndex = 13;
@@ -147,7 +147,7 @@
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(128, 263);
+            this.textBox4.Location = new System.Drawing.Point(12, 322);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(286, 26);
             this.textBox4.TabIndex = 14;
@@ -155,7 +155,7 @@
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(128, 314);
+            this.textBox5.Location = new System.Drawing.Point(12, 373);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(286, 26);
             this.textBox5.TabIndex = 15;
@@ -164,7 +164,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(212, 343);
+            this.label7.Location = new System.Drawing.Point(96, 402);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(28, 19);
             this.label7.TabIndex = 16;
@@ -174,7 +174,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(383, 343);
+            this.label8.Location = new System.Drawing.Point(267, 402);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(29, 19);
             this.label8.TabIndex = 17;
@@ -196,7 +196,7 @@
             "10",
             "11",
             "12"});
-            this.comboBox1.Location = new System.Drawing.Point(128, 365);
+            this.comboBox1.Location = new System.Drawing.Point(12, 424);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(53, 21);
             this.comboBox1.TabIndex = 18;
@@ -237,7 +237,7 @@
             "29",
             "30",
             "31"});
-            this.comboBox2.Location = new System.Drawing.Point(187, 365);
+            this.comboBox2.Location = new System.Drawing.Point(71, 424);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(53, 21);
             this.comboBox2.TabIndex = 19;
@@ -245,7 +245,7 @@
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(288, 365);
+            this.textBox6.Location = new System.Drawing.Point(172, 424);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(124, 26);
             this.textBox6.TabIndex = 20;
@@ -254,7 +254,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 535);
+            this.ClientSize = new System.Drawing.Size(318, 572);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
