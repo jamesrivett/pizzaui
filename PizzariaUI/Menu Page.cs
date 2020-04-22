2d25b679868644d534b9f1f66296d5b7e4b3a172
Nulious48 <54681424+Nulious48@users.noreply.github.com>
2020-04-21 21:40:38 -0400

    Lined Up

--- PizzariaUI/Menu Page.cs
+++ PizzariaUI/Menu Page.cs
@@ -16,5 +16,25 @@ namespace PizzariaUI
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
