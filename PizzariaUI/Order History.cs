819f42c8130751093597fcf463b5fac2d51c5437
jamesrivett <jamesrivett@users.noreply.github.com>
2020-04-22 00:02:21 -0400

    Order History Sidebar Added

--- PizzariaUI/Order History.cs
+++ PizzariaUI/Order History.cs
@@ -16,5 +16,25 @@ namespace PizzariaUI
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
